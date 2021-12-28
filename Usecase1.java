8501e13925347bda9efbae9bed05c9eeb49e592a
Vibha <vibhauc@gmail.com>
2021-12-28 21:53:33 +0530

    Message added to Usecase1.java

--- Usecase1.java
+++ Usecase1.java
@@ -3,7 +3,7 @@ public class Usecase1{
     public static void main(String[] args){
         int a = 1;
         int b = 2;
-        System.out.println("Welcome to Employee Wage Computation Program" , a+b);
+        System.out.println("Welcome to the Employee Wage Computation Program" , a+b);
 
     }
 }
