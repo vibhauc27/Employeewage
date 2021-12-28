0717962653954287ded5f0d820325bb94dd327a5
Vibha <vibhauc@gmail.com>
2021-12-28 21:31:34 +0530

    Initial commit

---
+++ Usecase1.java
@@ -0,0 +1,9 @@
+public class Usecase1{
+
+    public static void main(String[] args){
+        int a = 1;
+        int b = 2;
+        System.out.println("Welcome to Employee Wage Computation Program" , a+b);
+
+    }
+}
